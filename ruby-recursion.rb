f79ca4ae86904d66b1f713752748f82721932ba8
JLaRoc182 <47267449+JLaRoc182@users.noreply.github.com>
2022-01-14 15:52:17 -0500

    added merge-sort method to ruby-recursion.rb

--- ruby-recursion.rb
+++ ruby-recursion.rb
@@ -29,7 +29,32 @@ def fibs_rec(n)
 end
 
 
+#Merge-sort array method
+ def merge_sort(array)
+    #base case
+    return array if array.length < 2
+
+    #divide and conquer the array using recursion
+    endpoint = array.length
+    midpoint = array.length/2
+    p left_array = array[0...midpoint]
+    p right_array = array[midpoint..endpoint]
+    p left_sorted = merge_sort(left_array)
+    p right_sorted = merge_sort(right_array)
+
+    #merge the two already sorted arrays into a new combined sorted array (except for the last remaining value)
+    new_array = []
+    until left_sorted.empty? || right_sorted.empty? do  
+      left_sorted.first <= right_sorted.first ? new_array << left_sorted.shift : new_array << right_sorted.shift
+    end
+
+    #add that last remaining value to the new array and return it
+    return p (new_array + left_sorted + right_sorted) 
+  end
 
 
+
+
+my_array = [2, 4, 7, 5, 6, 1, 3, 8]
 
 binding-pry
